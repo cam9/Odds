a9aa74d19805223615f3035dc785a032b0bd58ae
luntc@bc.edu <luntc@bc.edu>
2015-04-02 17:01:30 -0400

    click listener sends text if odds are met

--- app/src/main/java/edu/bc/luntc/odds/MainActivity.java
+++ app/src/main/java/edu/bc/luntc/odds/MainActivity.java
@@ -2,38 +2,51 @@ package edu.bc.luntc.odds;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.telephony.SmsManager;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.Random;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    String message;
+    int percent;
+    String phone;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final TextView result = (TextView)findViewById(R.id.result);
+
+        Button button = (Button)findViewById(R.id.button);
+
+        button.setOnClickListener( new Button.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Random r = new Random();
+                percent = Integer.parseInt(((EditText) findViewById(R.id.percent)).getText().toString());
+                phone = ((EditText)findViewById(R.id.phone)).getText().toString();
+                message = ((EditText)findViewById(R.id.message)).getText().toString();
+
+                if(Math.abs(r.nextInt())%100 < percent){
+                    SmsManager.getDefault().sendTextMessage(phone, null, message, null, null);
+                    result.setText("SENT!!");
+                }
+                else{
+                    result.setText("You were lucky this time");
+                }
+            }
+        });
+
+
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
